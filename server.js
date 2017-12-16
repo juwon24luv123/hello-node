48836ebd2d1e09a959fd31ff67c6000b4d5b24ce
juwon24luv123 <juwon24luv1@yahoo.com>
2017-12-16 04:56:04 -0800

    this is our first commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+// path of link folder
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase()
+});
+
+// middleware
+
+app.use((req, res, next) => {
+    var now = new Date() .toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    
+    fs.appendFile('server.log', log + '\n');
+    next();
+});
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/private'));
+
+
+
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website',
+        currentYear: new Date().getFullYear()
+    });
+
+});
+
+    app.get('/about', (req, res) => {
+     res.render('About.hbs', {
+         pageTitle: 'About page',
+         currentYear: new Date().getFullYear()
+     });
+});
+
+    // bad - send back json with errorMesage
+// app.get('/bad', (req, res) => {
+//     res.send({
+//         errorMessage: 'Unable to handle request'
+//     });
+// });
+
+// app.get('/information', (req, res) => {
+//     res.send('Information about the page');
+// });
+
+// app.get('/contact', (req, res) => {
+//     res.send({
+//             success: 'success about the page'
+//     });
+// });
+
+app.listen(5000,() => {
+    console.log("listening on port 5000");
+});
